2d05b926958e4aa4552222a5c8f9c156e2fe99d7
yuntai <yuntai.kyong@gmail.com>
2022-02-14 18:30:38 +0900

    white space changes only

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# regex the "hostname" of the container into the 
+# regex the "hostname" of the container into the
 # eth-intelligence-api config and start eth-intelligence-api
 cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
@@ -14,7 +14,7 @@ geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 
 sleep 3
 
-# get IP address of the "miner" service, which we 
+# get IP address of the "miner" service, which we
 # set up as the bootnode for this network, and
 # merge it into the other options from "docker-compose.yml"
 BOOTSTRAP_IP=`getent hosts miner | cut -d" " -f1`
